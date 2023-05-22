ccfeca49b3035f81c25da14e9dc1cbdce70b067a
Nickita0000 <nzadubniak@gmail.com>
2023-05-22 22:56:46 +0100

    CRUD wout server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from './Form.js';
+import List from './List.js'
+import { useState } from "react";
+
+const initialList = [
+    {
+        "name":"Ольга",
+        "surname":"Профатилова",
+        "phone":"1",
+        "id":"42"
+    },
+    {
+        "name":"Ольга"
+        ,"surname":"Профатилова"
+        ,"phone":"5667"
+        ,"id":"43"
+    },
+    {
+        "name":"Ольга",
+        "surname":"Профатилова"
+        ,"phone":"8",
+        "id":"44"
+    },
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [list, setList] = useState(initialList)
+    const [userEdit, setUserEdit] = useState({})
+
+    function onFormSubmit(user) {
+        if(user.id){
+            const newUserList = list.map(userItem => userItem.id === user.id ? user : userItem)
+            setList(newUserList)
+        } else {
+            const userListFromServer = {
+                ...user,
+                id: Math.random()
+            }
+
+            setList([...list, userListFromServer])
+        }
+    }
+
+    function onUserListRemove(id) {
+        const updatedList = list.filter(user => user.id !== id)
+
+        setList(updatedList)
+    }
+
+    function onUserListEdit(user) {
+        setUserEdit(user)
+    }
+
+    return (
+      <div>
+        <Form
+            userEdit={userEdit}
+            onFormSubmit={onFormSubmit}
+        />
+        <List
+            list = {list}
+            onUserListRemove={onUserListRemove}
+            onUserListEdit={onUserListEdit}
+        />
+      </div>
+    );
 }
 
 export default App;
